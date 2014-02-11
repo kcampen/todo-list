54ba94e7b92b05b2a9891ac646920783b01d4e09
Katy Campen <katycampen@gmail.com>
2014-02-11 16:44:33 -0500

    restart todo list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,56 @@
-console.log('\'Allo \'Allo!');
+// var todoTemplate, description, todo;
+var todoList = [
+	{
+		description: 'wake up',
+		done: false,
+		id: _.uniqueId()
+	},{
+		description: 'eat pizza',
+		done: false,
+		id: _.uniqueId()
+	}
+]
+
+var todoTemplate = _.template($('.todo-template').text())
+
+console.log('ran template')
+
+_.each(todoList, function(item){
+	$('.todo-items').prepend(todoTemplate(item))
+})
+
+$('.js-add-todo').on('click', function(){
+
+	var description = $('.js-new-todo-input').val();
+	
+	var todo = {
+		description: description,
+		done: false,
+		id: _.uniqueId()
+	}
+
+	var renderedTemplate = todoTemplate(todo);
+
+$('.todo-items').prepend(renderedTemplate);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// 	// Function        Function     string
+// var todoTemplate = _.template($('.todo-template').text())
+	
+// 	// String           Function         Object
+// var renderedTemplate = todoTemplate({description: 'fake todo'});
+
+// // Jquery selector             string
+// $('.todo-items').prepend(renderedTemplate);
+// // reading right to left. 
