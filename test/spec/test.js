3fc7960e1c5bd88a9ef2836fed1e8571d7519d33
Mason Stewart <masondesu@gmail.com>
2014-02-12 21:07:43 -0500

    Clear out the todos before each test

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,6 +3,14 @@
 (function () {
     'use strict';
 
+    // before each it() function, do this
+    beforeEach(function(){
+        console.log('clearing out the tasks div!');
+
+        // clear out the tasks div
+        $('.todo-items').html('');
+    });
+
     describe('In the todo app', function () {
         describe('when the add button is clicked', function () {
             it('should add a new todo div when the input is populated', function () {
